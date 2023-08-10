b75c6ecd793e26f7130898a914f48f77af2f93c1
prabeshdulal <138851338+prabeshdulal@users.noreply.github.com>
2023-08-10 21:04:04 +0545

    Create IfElse.sol

---
+++ IfElse.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract structData{
+
+ //if else in solidity
+   function check() public returns (bool) {      
+    uint256 num = 34;
+    if(num < 34){
+        //do something
+        return false;
+    }else if(num == 34){
+        //do something
+        return true;
+    }else{
+        //do this
+        return false;
+    }
+    }
+   
+
+}
